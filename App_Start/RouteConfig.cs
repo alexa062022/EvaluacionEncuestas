1f29292c439febf0f468c3e41fe823b473a738d2
blzamorac <blzamorac@gmail.com>
2022-06-20 16:21:28 -0600

    se actualizan la version de Bootstrap y demás complementos

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EvaluacionServicios
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Escritorio", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Controllers/EscritorioController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EvaluacionServicios.Controllers
+{
+    public class EscritorioController : Controller
+    {
+        // GET: Escritorio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
